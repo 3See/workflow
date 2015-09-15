dfda1f4dcb30f6c86e2e78bc87afec8b157dc289
csturco0331 <csturco0331@yahoo.com>
2015-09-14 20:07:15 -0400

    Revert "Revert "work in progress""
    This reverts commit c713cb41d82756a8d82eaf88fe4c7ab042e6c0d5.

--- todo-src/script.js
+++ todo-src/script.js
@@ -6,63 +6,21 @@ var myApp = angular.module('app', []);
  
  
 myApp.controller('MainCtrl', function ($scope){ 
-  $scope.high = []; 
-  $scope.medium = []; 
-  $scope.low = []; 
+  $scope.todos = [];
   $scope.newItem = ""; 
     
   $scope.addItem = function(){ 
     console.log("add"); 
     if ($scope.newItem !== ""){
-      if ($scope.priority == "high") {$scope.high.push($scope.newItem);}
-      if ($scope.priority == "med") {$scope.medium.push($scope.newItem);}
-      if ($scope.priority == "low") {$scope.low.push($scope.newItem);}
-      $scope.newItem = ""; 
-    } 
+      $scope.todos.push($scope.newItem);}
+      $scope.newItem = "";
   } 
  
- 
-  $scope.highcheckItem = function(item){ 
-    console.log("high check"); 
-    var index = $scope.high.indexOf(item); 
-    //need implementation 
-  } 
- 
- 
-  $scope.highdeleteItem = function(item){ 
-    console.log("high delete"); 
-    var index = $scope.high.indexOf(item); 
-    $scope.high.splice(index, 1); 
-  } 
- 
- 
-  $scope.medcheckItem = function(item){ 
-    console.log("medium check"); 
-    var index = $scope.medium.indexOf(item); 
-    //need implementation 
-  } 
- 
- 
-  $scope.meddeleteItem = function(item){ 
-    console.log("med delete"); 
-    var index = $scope.medium.indexOf(item); 
-    $scope.medium.splice(index, 1); 
-  } 
- 
- 
-  $scope.highcheckItem = function(item){ 
-    console.log("low check"); 
-    var index = $scope.low.indexOf(item); 
-    //Need implementation 
-  } 
- 
- 
-  $scope.lowdeleteItem = function(item){ 
-    console.log("low delete"); 
-    var index = $scope.low.indexOf(item); 
-    $scope.low.splice(index, 1); 
-  } 
- 
+  $scope.deleteItem = function(item){ 
+    console.log("delete"); 
+    var index = $scope.todos.indexOf(item); 
+    $scope.todos.splice(index, 1); 
+  }
  
 }); 
  
