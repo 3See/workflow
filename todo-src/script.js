543f94d87f87225012b23a9246dddf02fedf3b9d
kgoins <goins.kyle@ufl.edu>
2015-09-16 00:13:13 -0400

    Update script.js

--- todo-src/script.js
+++ todo-src/script.js
@@ -5,7 +5,22 @@ var myApp = angular.module('app', []);
 myApp.controller('MainCtrl', function ($scope){
   $scope.todos = ["Learn Angular", "Learn node"];
   $scope.newItem = "";
+  //used to safeguard against undefined
+  var un_defined;
   
+$scope.addItem = function(){ 
+    console.log("add"); 
+    if ($scope.newItem !== "" && $scope.priority !== un_defined){
+      $scope.todos.push({name:$scope.newItem, priority:$scope.priority});}
+      $scope.todos.sort(function(a,b) {
+        if(a.priority > b.priority) {return 1;}
+        if(a.priority < b.priority) {return -1;}
+        return 0;
+      });
+      $scope.newItem = "";
+  } 
+
+
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
@@ -20,6 +35,30 @@ myApp.controller('MainCtrl', function ($scope){
     $scope.todos.splice(index, 1);
   }
     
+
+$scope.editItem = function(item, uInput, priority){
+    console.log("in edit");
+    var index = $scope.todos.indexOf(item);
+    if (uInput) {
+      $scope.todos[index].name = uInput;
+    }
+    else{
+      $scope.todos[index].name = $scope.todos[index].name;
+    }
+    $scope.uInput = "";
+    
+    $scope.todos[index].priority = priority;
+    //if (1){
+      //$scope.todos.splice(index, 1); 
+      //$scope.todos.push({name:uInput, priority:priority});}
+      $scope.todos.sort(function(a,b) {
+        if(a.priority > b.priority) {return 1;}
+        if(a.priority < b.priority) {return -1;}
+        return 0;
+      });
+
+  }
+
   
 });
 
